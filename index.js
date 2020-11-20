8df0c4f0587f742647bf619a467adec3830a9b17
Felix-Suen <felix.suen2000@gmail.com>
2020-11-19 16:03:23 -0800

    finding prices of homes

--- index.js
+++ index.js
@@ -9,25 +9,27 @@ let opts = {
     LongitudeMax: -79.6079635620117,
     LatitudeMin: 43.57601549736786,
     LatitudeMax: 43.602250137362276,
-    PriceMin: 10000,
+    PriceMin: 100000,
     PriceMax: 410000,
+    CurrentPage: 1
 };
 
 app.get('/', (req, res) => {
     res.send(
         realtor.optionsFromUrl(
             'https://www.realtor.ca/Residential/Map.aspx#LongitudeMin=-79.6758985519409&LongitudeMax=-79.6079635620117&LatitudeMin=43.57601549736786&LatitudeMax=43.602250137362276&PriceMin=100000&PriceMax=425000'
-        )
+            )
     );
 });
 
 realtor
     .post(opts)
     .then((data) => {
-        console.log(data);
+        //console.log(data);
+        data.Results.map(result => console.log(result.Property.Price))
     })
     .catch((err) => {console.error(err.message)});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}!`);
+    console.log(`Testing app listening on port ${port}!`);
 });
