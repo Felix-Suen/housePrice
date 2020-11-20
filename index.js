d4978af5eeb0a0ef5d4e8972cbdca4f6864ae448
Felix-Suen <felix.suen2000@gmail.com>
2020-11-19 16:53:58 -0800

    localhost

--- index.js
+++ index.js
@@ -11,32 +11,32 @@ let opts = {
     LatitudeMax: 43.602250137362276,
     PriceMin: 100000,
     PriceMax: 1000000,
-    RecordsPerPage: 100
+    RecordsPerPage: 10,
+    // TotalPage: 45,
+    // Pins: 438,
+    // CurrentPage: 1,
 };
 
-app.get('/', (req, res) => {
-    res.send(realtor
-    .post(opts)
-    .then((data) => {
-        // console.log(data);
-        data.Results.map(result => console.log(result.Property.Price))
-    })
-    .catch((err) => {console.error(err.message)}));
-    
-    res.send(
-        realtor.optionsFromUrl(
-            'https://www.realtor.ca/Residential/Map.aspx#LongitudeMin=-79.6758985519409&LongitudeMax=-79.6079635620117&LatitudeMin=43.57601549736786&LatitudeMax=43.602250137362276&PriceMin=100000&PriceMax=425000'
-            )
-    );
-});
-
+// console display
 realtor
     .post(opts)
     .then((data) => {
-        // console.log(data);
-        data.Results.map(result => console.log(result.Property.Price))
+        console.log(data);
+        // data.Results.map(result => console.log(result.Property))
     })
     .catch((err) => {console.error(err.message)});
+
+
+// localhost:3000
+var test = {};
+
+realtor.post(opts).then((data) => {
+    test = data.Results.map(result => result.Property.Price)
+});
+
+app.get('/', (req, res)=> { 
+    res.send(test);
+})
 
 app.listen(port, () => {
     console.log(`Testing app listening on port ${port}!`);
