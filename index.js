41968141a9b96435da5c8e7f6601eea20bfe5c85
Felix-Suen <felix.suen2000@gmail.com>
2020-12-16 15:01:16 -0800

    parsed int data

--- index.js
+++ index.js
@@ -38,14 +38,31 @@ realtor
     .post(opts)
     .then((data) => {
         excelData = data.Results.map((result) => {
+            var bedrooms = result.Building.Bedrooms.split(" + ");
+            var numBed = 0;
+            if (bedrooms.length > 1) {
+                numBed = parseInt(bedrooms[0]) + parseInt(bedrooms[1]);
+            } else {
+                numBed = parseInt(bedrooms[0]);
+            }
+
+            var type = 0;
+            if (result.Building.Type === 'Apartment') type = 1;
+
+            var parking = 0;
+            if (result.Property.ParkingSpaceTotal) {
+                parking = parseInt(result.Property.ParkingSpaceTotal);
+            }
+
             var row = [
                 result.Property.Address.AddressText,
-                result.Building.Bedrooms,
+                numBed,
                 parseInt(result.Building.BathroomTotal),
-                result.Building.Type,
-                parseInt(result.Property.ParkingSpaceTotal),
+                type,
+                parking,
                 parseInt(result.Property.PriceUnformattedValue),
             ];
+            console.log(row);
             return row;
         });
         excel();
